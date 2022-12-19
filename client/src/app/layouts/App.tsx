7dd7c94a06214531af924670c0e16f116b49413a
kouzber123 <48252892+kouzber123@users.noreply.github.com>
2022-12-19 15:03:50 +0200

    end of section 4 > added react routers

--- client/src/app/layouts/App.tsx
+++ client/src/app/layouts/App.tsx
@@ -4,7 +4,12 @@ import CssBaseline from "@mui/material/CssBaseline";
 
 import Container from "@mui/system/Container";
 import { useState } from "react";
+import { Route } from "react-router-dom";
+import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
 
 import Header from "./Header";
 
@@ -25,13 +30,19 @@ function App() {
     }
   });
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
-      <Container>
-        <Catalog />
-      </Container>
-    </ThemeProvider>
+   
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+        <Container>
+          <Route exact path={"/"} component={HomePage} />
+          <Route exact path={"/catalog"} component={Catalog} />
+          <Route path={"/catalog/:id"} component={ProductDetails} />
+          <Route path={"/about"} component={AboutPage} />
+          <Route path={"/contact"} component={ContactPage} />
+        </Container>
+      </ThemeProvider>
+    
   );
 }
 
--- client/src/app/layouts/Header.tsx
+++ client/src/app/layouts/Header.tsx
@@ -1,19 +1,71 @@
+import { ShoppingCart } from "@mui/icons-material";
+import { Badge, IconButton, List, ListItem } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Switch from "@mui/material/Switch";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import { Box } from "@mui/system";
+import { NavLink } from "react-router-dom";
 
 interface Props {
   darkMode: boolean;
   handleThemeChange: () => void;
 }
+
+const midLinks = [
+  { title: "catalog", path: "/catalog" },
+  { title: "about", path: "/about" },
+  { title: "contact", path: "/contact" }
+];
+const rightLinks = [
+  { title: "login", path: "/login" },
+  { title: "register", path: "/register" }
+];
+
+const navStyles = {
+  color: "inherit",
+  textDecoration: "none",
+  typography: "h6",
+  "&:hover": {
+    color: "grey.500"
+  },
+  "&.active": {
+    color: "text.secondary"
+  }
+};
 export default function Header({ darkMode, handleThemeChange }: Props) {
   return (
     <AppBar position="static" sx={{ mb: 4 }}>
-      <Toolbar>
-        <Typography variant="h6">RE-STORE</Typography>
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+        <Box display="flex" alignItems="center">
+          <Typography variant="h6" component={NavLink} to="/" exact sx={navStyles}>
+            RE-STORE
+          </Typography>
+          <Switch color="default" checked={darkMode} onChange={handleThemeChange} />
+        </Box>
 
-        <Switch color="default" checked={darkMode} onChange={handleThemeChange} />
+        <List sx={{ display: "flex" }}>
+          {midLinks.map(({ title, path }) => (
+            <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
+              {title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
+
+        <Box display="flex" alignItems="center">
+          <IconButton size="large" sx={{ color: "inherit" }}>
+            <Badge badgeContent={4} color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+          <List sx={{ display: "flex" }}>
+            {rightLinks.map(({ title, path }) => (
+              <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Toolbar>
     </AppBar>
   );
